e71fd7c7735b94076c9fc5f974cc6e804d808259
rorschachkwok <564931539@qq.com>
2022-04-16 19:39:41 +0800

    character table

--- table_character.R
+++ table_character.R
@@ -3,10 +3,13 @@ pacman::p_load(
         openxlsx,
         coin,
         epikit,
-        tidyverse
+        tidyverse,
+        janitor
 )
 
 df <- read.xlsx('cleaned_data.xlsx')
+
+# make age groups, vac status classify as at most partially and fully
 cat_ll <- df %>% 
         mutate(
                 age_cat = age_categories(             # create new column
@@ -17,60 +20,61 @@ cat_ll <- df %>%
                         index_age,                            # numeric column to make groups from
                         breakers = c(0, 10,  20,        # break points
                                      30, 40, 50, 60))) %>% 
-        mutate(vac_history = replace(vac_history, vac_history == 1, 0))
+        mutate(vac_history = replace(vac_history, vac_history == 1, 0)) %>% 
+        mutate(across(.cols = c(vac_history, gender),
+                      .fns = as.factor)) %>% 
+        mutate(days_vac_exposure = as.numeric(days_vac_exposure),
+               clnc_outcome      = as.numeric(clnc_outcome))
+table(cat_ll$age_cat)
 
 
-sele <- cat_ll %>% 
+
+# select           PCR positive           close contacts
+sele_p <- cat_ll %>% 
+        filter(outcome == 1)
+
+# see the age distribution of at most partially vac and PCR positive close contacts
+sele_p_amp <- sele_p %>% filter(vac_history == 0)
+hist(sele_p_amp$age)
+
+# stat-test PCR positive close contacts
+wilcox.test(age ~ vac_history, sele_p)
+# median_test(age ~ vac_history, mid.score = '0.5', data = sele_p)
+chisq.test(sele_p$gender, sele_p$vac_history)
+wilcox.test(clnc_outcome ~ vac_history, sele_p)
+
+# select PCR positive and fully vaccinated in close contacts
+sele_p_f <- cat_ll %>% 
         filter(outcome == 1) %>% 
-        filter(vac_history == 2) %>% 
-        mutate(days_vac_exposure = as.numeric(days_vac_exposure)) 
-        # group_by(age_cat) %>% 
+        filter(vac_history == 2)
+        # group_by(age_cat) %>%
         # summarise(iqr2 = median(days_vac_exposure),
         #           iqr1 = quantile(days_vac_exposure, probs = 0.25),
         #           iqr3 = quantile(days_vac_exposure, probs = 0.75))
 
-sele_p <- cat_ll %>% 
-        filter(outcome == 1) %>% 
-        mutate(across(.cols = c(vac_history, gender, clnc_outcome),
-                      .fns = as.factor))
+# test median time from fully-vac to exposure between age groups
+kruskal.test(days_vac_exposure ~ age_cat, sele_p_f)
+# median_test(days_vac_exposure ~ age_cat, mid.score = '0.5', data = sele_p_f)
 
-median_test(age ~ vac_history, mid.score = '0.5', data = sele_p)
-sele_p <- sele_p %>% 
-        filter(vac_history == 2)
-median_test(days_vac_exposure ~ age_cat, mid.score = '0.5', data = sele_p)
-
-chisq.test(sele_p$gender, sele_p$vac_history)
-chisq.test(sele_p$clnc_outcome, sele_p$vac_history, simulate.p.value = T)
-
-sele_p %>% 
-        filter(vac_history == 2) %>% 
-        t_test(days_vac_exposure ~ age_cat)
-
+# select PCR negative close contacts
 sele_n <- cat_ll %>% 
-        filter(outcome == 0) %>% 
-        mutate(across(.cols = c(vac_history, gender, clnc_outcome),
-                      .fns = as.factor)) 
-sele_n %>% 
-        t_test(age ~ vac_history)
-
-median_test(age ~ vac_history,  data = sele_n)
-sele_n <- sele_n %>% 
-        filter(vac_history == 2)
-median_test(days_vac_exposure ~ age_cat, data = sele_n)
-
+        filter(outcome == 0) 
+# stat-test PCR negative close contacts
 chisq.test(sele_n$gender, sele_n$vac_history)
-
-sele_n %>% 
-        filter(vac_history == 2) %>% 
-        # filter(age_cat != '0-9') %>% 
-        t_test(age_cat ~ days_vac_exposure)
-
+# median_test(age ~ vac_history, mid.score = '0.5', data = sele_n)
+wilcox.test(age ~ vac_history, sele_n)
+# select PCR negative and fully vaccinated in close contacts
+sele_n_f <- sele_n %>% 
+        filter(vac_history == 2)
+kruskal.test(days_vac_exposure ~ age_cat, data = sele_n_f)
+# median_test(days_vac_exposure ~ age_cat, data = sele_n_f)
+# sele_n_amp <- sele_n %>% filter(vac_history == 0)
+# hist(sele_n_amp$age)
 
 
 
 
 quantile(sele$days_vac_exposure, probs = seq(0, 1, 0.25))        
-library(janitor)
 df %>% 
         mutate(index_days_onset_quar = as.factor(index_days_onset_quar)) %>% 
         tabyl(index_days_onset_quar) %>% 
@@ -85,6 +89,13 @@ sele <- df %>%
         select(at_most, age)
 sele %>% 
         tabyl(clnc_outcome, at_most) %>% 
+        adorn_percentages(denominator = 'col') %>% 
+        adorn_pct_formatting() %>% 
+        adorn_ns(position = 'front')
+
+sele_n %>% 
+        tabyl(gender, vac_history) %>% 
+        adorn_totals(where = 'both') %>% 
         adorn_percentages(denominator = 'col') %>% 
         adorn_pct_formatting() %>% 
         adorn_ns(position = 'front')
